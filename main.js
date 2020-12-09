03e8c3293408334ef06c29e4dc9f3e431e2f010f
Srdjan Sobat <sshobat@gmail.com>
2020-12-09 10:32:07 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+//TASK 1
+
+var peterFunds,
+    jimFunds;
+
+peterFunds = 3500;
+jimFunds = 5000;
+
+console.log(`Peter has more money than Jim!`, peterFunds > jimFunds);
+
+//TASK 2
+
+var johnAge,
+    mikeAge;
+
+johnAge = 38;
+mikeAge = (johnAge + 8) / 2 - 8;
+
+console.log(`Mike is ${mikeAge} years old.`);
+
+//TASK 3
+
+var firstFlock,
+    secondFlock;
+
+firstFlock = 78;
+secondFlock = 57;
+
+firstFlock -= 12;
+secondFlock += 12;
+
+secondFlock -= 4;
+
+console.log(`The first flock is larger or equal to the second one!`, firstFlock >= secondFlock);
+
+
+
+
